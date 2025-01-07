00579c26aad05fcc85c09742ae654892e1f85d6e
Charlie Whitfield <charliewhitfield@zoho.com>
2025-01-07 09:21:12 -0500

    Update copyright for 2025

--- public/net_components/cyberspace_net.gd
+++ public/net_components/cyberspace_net.gd
@@ -2,7 +2,7 @@
 # This file is part of Astropolis
 # https://t2civ.com
 # *****************************************************************************
-# Copyright 2019-2024 Charlie Whitfield; ALL RIGHTS RESERVED
+# Copyright 2019-2025 Charlie Whitfield; ALL RIGHTS RESERVED
 # Astropolis is a registered trademark of Charlie Whitfield in the US
 # *****************************************************************************
 class_name CyberspaceNet
