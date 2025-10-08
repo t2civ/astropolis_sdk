897b950296d3d151bb82f74da6297a9225372364
Charlie Whitfield <charliewhitfield@zoho.com>
2025-10-08 10:18:53 -0400

    Fixes for Core plugin changes

--- public/gui_panels/selection_panel.gd
+++ public/gui_panels/selection_panel.gd
@@ -16,18 +16,3 @@ const PERSIST_PROPERTIES: Array[StringName] = [
 	&"anchor_right",
 	&"anchor_bottom",
 ]
-
-
-var default_view_name := &"LABEL_VIEW1" # will increment if taken
-var collection_name := &"SP"
-var is_cached := false # if false, persisted via gamesave
-var view_flags := ViewFlags.VIEWFLAGS_ALL
-var init_flags := ViewFlags.VIEWFLAGS_ALL_CAMERA
-var reserved_view_names: Array[StringName] = [&"BUTTON_HOME"]
-
-
-
-func _ready() -> void:
-	@warning_ignore("unsafe_method_access")
-	$"%ViewSaveFlow".init($"%ViewSaveButton", default_view_name, collection_name, is_cached,
-			view_flags, init_flags, reserved_view_names)
