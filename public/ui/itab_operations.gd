89e47299cedc306a0e8c1c93da7a8d85c4e20af2
Charlie Whitfield <charliewhitfield@zoho.com>
2026-03-03 12:17:51 -0500

    Recode Operations tab w/ FoldableContainers

--- public/ui/itab_operations.gd
+++ public/ui/itab_operations.gd
@@ -33,10 +33,7 @@ const PROCESS_GROUP_EXTRACTION := Enums.ProcessGroup.PROCESS_GROUP_EXTRACTION
 
 const N_COLUMNS := 7
 
-const OPEN_PREFIX := "\u2304   "
-const CLOSED_PREFIX := ">   "
-const SINGULAR_PREFIX := "     "
-const SUB_PREFIX := "         "
+const SUBGROUP_INDENT := 17
 
 const PERSIST_MODE := IVGlobal.PERSIST_PROCEDURAL
 const PERSIST_PROPERTIES: Array[StringName] = [
@@ -49,12 +46,23 @@ var current_tab := 0
 var _on_ready_tab := 0
 
 # not persisted
+var base_column_width := 55.0
+
+var headers_texts: Array[Array] = [
+	["Capacity\n(%)", "Power\n(MW)", "Fuel\n(-t/h)", "Revenue\n($M/y)", "Margin\n(gr; %)"],
+	["Capacity\n(%)", "Power\n(-MW)", "Rate\n(t/h)", "Revenue\n($M/y)", "Margin\n(gr; %)"],
+	["Capacity\n(%)", "Power\n(-MW)", "Rate\n(t/h)", "Revenue\n($M/y)", "Margin\n(gr; %)"],
+	["Capacity\n(%)", "Power\n(-MW)", "Rate\n(t/h)", "Revenue\n($M/y)", "Margin\n(gr; %)"],
+	["Capacity\n(%)", "Power\n(-MW)", "Bioprod\n(t/h)", "Revenue\n($M/y)", "Margin\n(gr; %)"],
+	["Capacity\n(%)", "Power\n(-MW)", "Compute\n(Pflop/s)", "Revenue\n($M/y)", "Margin\n(gr; %)"],
+]
+
 
 var _unit_multipliers := IVUnits.unit_multipliers
 var _selection_manager: SelectionManager
 var _suppress_tab_listener := true
 
-var _name_column_width := 250.0 # TODO: resize on GUI resize (also in RowItem)
+#var _name_column_width := 250.0 # TODO: resize on GUI resize (also in RowItem)
 
 # table indexing
 var _db_tables := IVTableData.db_tables
@@ -68,11 +76,24 @@ var _op_group_show_singular: Array[bool] = _db_tables[&"op_groups"][&"show_singu
 var _op_classes_op_groups: Array[Array] = _tables_aux[&"op_classes_op_groups"]
 var _op_groups_operations: Array[Array] = _tables_aux[&"op_groups_operations"]
 
+var _revenue_hdrs: Array[Label] = []
+var _margin_hdrs: Array[Label] = []
+
+var _fold_icon_substitute := MeshTexture.new()
 
 @warning_ignore("unsafe_property_access")
 @onready var _memory: Dictionary = get_parent().memory # open states
 @onready var _no_ops_label: Label = $NoOpsLabel
 @onready var _tab_container: TabContainer = $TabContainer
+@onready var _headers: Array[HBoxContainer] = [
+	%EnergyHdrs,
+	%ExtractionHdrs,
+	%RefiningHdrs,
+	%ManufacturingHdrs,
+	%BiomesHdrs,
+	%ServicesHdrs,
+]
+
 @onready var _vboxes: Array[VBoxContainer] = [
 	$"%EnergyVBox",
 	$"%ExtractionVBox",
@@ -81,30 +102,8 @@ var _op_groups_operations: Array[Array] = _tables_aux[&"op_groups_operations"]
 	$"%BiomesVBox",
 	$"%ServicesVBox",
 ]
-@onready var _col0_spacers: Array[Control] = [
-	$TabContainer/Energy/Hdrs/Spacer,
-	$TabContainer/Extraction/Hdrs/Spacer,
-	$TabContainer/Refining/Hdrs/Spacer,
-	$TabContainer/Manufacturing/Hdrs/Spacer,
-	$TabContainer/Biomes/Hdrs/Spacer,
-	$TabContainer/Services/Hdrs/Spacer,
-]
-@onready var _revenue_hdrs: Array[Label] = [
-	$TabContainer/Energy/Hdrs/Hdr4,
-	$TabContainer/Extraction/Hdrs/Hdr4,
-	$TabContainer/Refining/Hdrs/Hdr4,
-	$TabContainer/Manufacturing/Hdrs/Hdr4,
-	$TabContainer/Biomes/Hdrs/Hdr4,
-	$TabContainer/Services/Hdrs/Hdr4,
-]
-@onready var _margin_hdrs: Array[Label] = [
-	$TabContainer/Energy/Hdrs/Hdr5,
-	$TabContainer/Extraction/Hdrs/Hdr5,
-	$TabContainer/Refining/Hdrs/Hdr5,
-	$TabContainer/Manufacturing/Hdrs/Hdr5,
-	$TabContainer/Biomes/Hdrs/Hdr5,
-	$TabContainer/Services/Hdrs/Hdr5,
-]
+
+
 
 
 func _ready() -> void:
@@ -121,8 +120,31 @@ func _ready() -> void:
 	$TabContainer/Biomes.name = &"TAB_OPS_BIOMES"
 	$TabContainer/Services.name = &"TAB_OPS_SERVICES"
 	
-	for col0_spacer in _col0_spacers:
-		col0_spacer.custom_minimum_size.x = _name_column_width - 10.0
+	_revenue_hdrs.resize(6)
+	_margin_hdrs.resize(6)
+	
+	for tab in 6:
+		var header_texts := headers_texts[tab]
+		var tab_headers := _headers[tab]
+		tab_headers.add_spacer(true)
+		for label_column in 5:
+			var label := Label.new()
+			tab_headers.add_child(label)
+			label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+			label.text = header_texts[label_column]
+			if label_column == 3:
+				_revenue_hdrs[tab] = label
+			elif label_column == 4:
+				_margin_hdrs[tab] = label
+		var spacer := Control.new()
+		spacer.size_flags_horizontal = SIZE_SHRINK_CENTER
+		tab_headers.add_child(spacer)
+	
+	_fold_icon_substitute.image_size.x = 16
+	
+	var gui_size: int = IVSettingsManager.get_setting(&"gui_size")
+	_resize(gui_size)
+	IVSettingsManager.changed.connect(_settings_listener)
 	
 	_tab_container.set_current_tab(_on_ready_tab)
 	_suppress_tab_listener = false
@@ -163,6 +185,23 @@ func _update_tab(_dummy := false) -> void:
 func _update_no_operations() -> void:
 	_tab_container.hide()
 	_no_ops_label.show()
+
+
+func _resize(gui_size: int) -> void:
+	const SCROLL_CORRECTION := 7.0
+	var column_width := base_column_width * IVCoreSettings.gui_size_multipliers[gui_size]
+	for tab in _headers.size():
+		var tab_headers := _headers[tab]
+		for i in 6:
+			var control: Control = tab_headers.get_child(i + 1)
+			var width := column_width if i < 5 else column_width + SCROLL_CORRECTION
+			control.custom_minimum_size.x = width
+
+
+func _settings_listener(setting: StringName, value: Variant) -> void:
+	if setting == &"gui_size":
+		var gui_size: int = value
+		_resize(gui_size)
 
 
 # *****************************************************************************
@@ -294,26 +333,26 @@ func _update_tab_display(target_name: StringName, tab: int, n_op_groups: int, ha
 	revenue_hdr.text = "Revenue\n($M/y)" if has_financials else ""
 	margin_hdr.text = "Margin\n(% gr)" if has_financials else ""
 	
-	# make GroupBoxes as needed
+	# make GroupFoldables as needed
 	var vbox: VBoxContainer = _vboxes[tab]
 	var n_children := vbox.get_child_count()
 	while n_children < n_op_groups:
-		vbox.add_child(GroupBox.new(_memory))
+		vbox.add_child(GroupFoldable.new(_memory, base_column_width, _fold_icon_substitute))
 		n_children += 1
 	
-	# set and show GroupBoxes
+	# set and show GroupFoldables
 	var i := 0
 	while i < n_op_groups:
 		var group_data: Array = data[i * 2]
 		var operations_data: Array = data[i * 2 + 1]
-		var group_box: GroupBox = vbox.get_child(i)
+		var group_box: GroupFoldable = vbox.get_child(i)
 		group_box.set_group_item(target_name, group_data, operations_data)
 		group_box.show()
 		i += 1
 	
 	# hide unused
 	while i < n_children:
-		var group_box: GroupBox = vbox.get_child(i)
+		var group_box: GroupFoldable = vbox.get_child(i)
 		group_box.hide()
 		i += 1
 	
@@ -322,75 +361,81 @@ func _update_tab_display(target_name: StringName, tab: int, n_op_groups: int, ha
 
 
 
-class GroupBox extends VBoxContainer:
+class GroupFoldable extends FoldableContainer:
 	# Reused container for RowItems
 	
-	var _group_hdr := RowItem.new(true)
-	var _is_open: bool
+	var _foldable_vbox := VBoxContainer.new()
+	var _group_hdr: RowItem
 	var _is_singular: bool
 	var _memory: Dictionary
+	var _base_column_width: float
 	var _memory_key: String
+	var _fold_icon_substitute: MeshTexture
 	
 	
-	func _init(memory: Dictionary) -> void:
+	func _init(memory: Dictionary, base_column_width: float, fold_icon_substitute: MeshTexture
+			) -> void:
 		_memory = memory
+		_base_column_width = base_column_width
+		_fold_icon_substitute = fold_icon_substitute
+		_group_hdr = RowItem.new(true, base_column_width)
 		size_flags_horizontal = SIZE_FILL
-		add_child(_group_hdr)
-		_group_hdr.group_button.button_down.connect(_toggle_open_close)
+		add_child(_foldable_vbox)
+		add_title_bar_control(_group_hdr)
+		folding_changed.connect(_on_folding_changed)
 	
 	
-	func set_group_item(_target_name: StringName, group_data: Array, operations_data: Array) -> void:
+	func set_group_item(_target_name: StringName, group_data: Array, operations_data: Array
+			) -> void:
 		_memory_key = group_data[0]
-		_is_open = _memory.get(_memory_key, false)
+		folded = _memory.get(_memory_key, true)
 		
 		var group_state: int
 		if operations_data:
-			group_state = GROUP_OPEN if _is_open else GROUP_CLOSED
+			group_state = GROUP_OPEN if !folded else GROUP_CLOSED
+			remove_theme_icon_override(&"folded_arrow")
 			_is_singular = false
 		else:
 			group_state = GROUP_SINGULAR
+			add_theme_icon_override(&"folded_arrow", _fold_icon_substitute)
 			_is_singular = true
+		var row_name: StringName = group_data[0]
+		title = row_name
 		_group_hdr.set_row(group_data, group_state)
 		
 		var n_ops := operations_data.size()
-		var n_children := get_child_count()
-		var n_children_needed := n_ops + 1
+		var n_children := _foldable_vbox.get_child_count()
+		var n_children_needed := n_ops
 		while n_children < n_children_needed:
-			add_child(RowItem.new(false))
+			_foldable_vbox.add_child(RowItem.new(false, _base_column_width))
 			n_children += 1
 		var i := 0
 		while i < n_ops:
-			var ops_row: RowItem = get_child(i + 1)
+			var ops_row: RowItem = _foldable_vbox.get_child(i)
 			var ops_datum: Array = operations_data[i]
 			ops_row.set_row(ops_datum)
-			ops_row.visible = _is_open
+			ops_row.show()
 			i += 1
 		
 		# hide unused
-		i = n_children_needed
+		i = n_ops
 		while i < n_children:
-			var ops_row: RowItem = get_child(i)
+			var ops_row: RowItem = _foldable_vbox.get_child(i)
 			ops_row.hide()
 			i += 1
 	
 	
-	func _toggle_open_close() -> void:
-		if _is_singular:
+	func _on_folding_changed(is_folded_: bool) -> void:
+		if !_is_singular:
+			_memory[_memory_key] = is_folded_
 			return
-		_is_open = !_is_open
-		_memory[_memory_key] = _is_open
-		_group_hdr.set_group_open_close(_is_open)
-		var n_children := get_child_count()
-		var i := 1
-		while i < n_children:
-			var ops_row: RowItem = get_child(i)
-			ops_row.visible = _is_open
-			i += 1
+		if !is_folded_:
+			fold()
+
 
 
 class RowItem extends HBoxContainer:
 	
-	var group_button: Button # if _is_group == true
 	var ops_label: Label # if _is_group == false
 	var utilization_label := Label.new()
 	var power_label := Label.new()
@@ -399,66 +444,50 @@ class RowItem extends HBoxContainer:
 	var margin_label := Label.new()
 	var controler := Control.new() # TODO
 	
-	
 	var _is_group: bool
-	var _group_name: StringName
-	var _name_column_width := 250.0 # TODO: resize on GUI resize
+	var _base_column_width: float
 	
-	
-	func _init(is_group: bool) -> void:
+	func _init(is_group: bool, base_column_width: float) -> void:
 		_is_group = is_group
+		_base_column_width = base_column_width
 		size_flags_horizontal = SIZE_FILL
 		
-		if is_group:
-			group_button = Button.new()
-			group_button.size_flags_horizontal = SIZE_EXPAND_FILL
-			group_button.custom_minimum_size.x = _name_column_width
-			group_button.flat = true
-			group_button.alignment = HORIZONTAL_ALIGNMENT_LEFT
-			add_child(group_button)
-		else:
+		if !is_group:
+			var spacer := Control.new()
+			spacer.size_flags_horizontal = SIZE_SHRINK_BEGIN
+			spacer.custom_minimum_size.x = SUBGROUP_INDENT
+			add_child(spacer)
 			ops_label = Label.new()
 			ops_label.size_flags_horizontal = SIZE_EXPAND_FILL
-			ops_label.custom_minimum_size.x = _name_column_width
 			add_child(ops_label)
-		utilization_label.size_flags_horizontal = SIZE_EXPAND_FILL
 		utilization_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-		power_label.size_flags_horizontal = SIZE_EXPAND_FILL
 		power_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-		flow_label.size_flags_horizontal = SIZE_EXPAND_FILL
 		flow_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-		revenue_label.size_flags_horizontal = SIZE_EXPAND_FILL
 		revenue_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-		margin_label.size_flags_horizontal = SIZE_EXPAND_FILL
 		margin_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-		controler.size_flags_horizontal = SIZE_FILL
-		controler.custom_minimum_size.x = 20
 		add_child(utilization_label)
 		add_child(power_label)
 		add_child(flow_label)
 		add_child(revenue_label)
 		add_child(margin_label)
 		add_child(controler)
+		var gui_size: int = IVSettingsManager.get_setting(&"gui_size")
+		_resize(gui_size)
+		IVSettingsManager.changed.connect(_settings_listener)
 	
 	
-	func set_row(data: Array, group_state := -1) -> void:
+	func set_row(data: Array, _group_state := -1) -> void:
 		# NAN, blank
 		# INF, "?"
-		var row_name: StringName = data[0]
+		
+		if !_is_group:
+			var row_name: StringName = data[0]
+			ops_label.text = row_name
 		var utilization: float = data[1]
 		var power: float = data[2]
 		var flow: float = data[3]
 		var revenue: float = data[4]
 		var margin: float = data[5]
-
-		if _is_group:
-			_group_name = row_name
-			if group_state == GROUP_SINGULAR:
-				group_button.text = SINGULAR_PREFIX + tr(row_name)
-			else:
-				set_group_open_close(group_state == GROUP_OPEN)
-		else:
-			ops_label.text = SUB_PREFIX + tr(row_name)
 		
 		utilization_label.text = "%.f" % (100.0 * utilization)
 		
@@ -491,9 +520,17 @@ class RowItem extends HBoxContainer:
 			margin_label.text = "%.f" % (100.0 * margin)
 	
 	
-	func set_group_open_close(is_open: bool) -> void:
-		assert(_is_group)
-		if is_open:
-			group_button.text = OPEN_PREFIX + tr(_group_name)
-		else:
-			group_button.text = CLOSED_PREFIX + tr(_group_name)
+	func _resize(gui_size: int) -> void:
+		var column_width := _base_column_width * IVCoreSettings.gui_size_multipliers[gui_size]
+		utilization_label.custom_minimum_size.x = column_width
+		power_label.custom_minimum_size.x = column_width
+		flow_label.custom_minimum_size.x = column_width
+		revenue_label.custom_minimum_size.x = column_width
+		margin_label.custom_minimum_size.x = column_width
+		controler.custom_minimum_size.x = column_width
+	
+	
+	func _settings_listener(setting: StringName, value: Variant) -> void:
+		if setting == &"gui_size":
+			var gui_size: int = value
+			_resize(gui_size)
